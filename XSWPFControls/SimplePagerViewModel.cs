b7c4257424ec1a752a7aa43b47cf4436243a8568
xs.zhou <newlifechou@outlook.com>
2016-04-08 08:55:54 +0800

    add simplepager control

---
+++ XSWPFControls/SimplePagerViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+    Developer:xs.zhou@outlook.com
+    CreateTime:2016/4/8 8:52:13
+*/
+namespace XSWPFControls
+{
+    public class SimplePagerViewModel:NotificationObject
+    {
+
+    }
+}
