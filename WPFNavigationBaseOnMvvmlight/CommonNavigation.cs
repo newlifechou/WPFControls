714e454c59893ba117d282b31c1aabeb8ab41650
xs.zhou <newlifechou@outlook.com>
2016-12-30 09:13:37 +0800

    create a public dll for wpf navigation base on mvvmlight

---
+++ WPFNavigationBaseOnMvvmlight/CommonNavigation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight.Messaging;
+
+namespace WPFNavigationBaseOnMvvmlight
+{
+    public static class CommonNavigation
+    {
+        public static void GoToNavigation(string viewName)
+        {
+            Messenger.Default.Send<string>(viewName, NavigationEnum.Navigate);
+        }
+    }
+}
