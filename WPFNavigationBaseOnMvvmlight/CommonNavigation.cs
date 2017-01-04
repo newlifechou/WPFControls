57751cf57b21c540d39fb6f1575c2a28917c7b44
xs.zhou <newlifechou@outlook.com>
2017-01-04 15:03:58 +0800

    add basic resources to wpfskin

--- WPFNavigationBaseOnMvvmlight/CommonNavigation.cs
+++ WPFNavigationBaseOnMvvmlight/CommonNavigation.cs
@@ -9,6 +9,10 @@ namespace WPFNavigationBaseOnMvvmlight
 {
     public static class CommonNavigation
     {
+        /// <summary>
+        /// 通用导航，通过viewName来区分导航页面，不考虑参数传递
+        /// </summary>
+        /// <param name="viewName"></param>
         public static void GoToNavigation(string viewName)
         {
             Messenger.Default.Send<string>(viewName, NavigationEnum.Navigate);
